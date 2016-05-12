68600b5be75c3bcc211e20f36561f04eaead5265
Raymond Benefield <GodlyPerfection@ReachingPerfection.com>
2016-05-12 10:24:17 -0700

    Added initial bash scripting setup.

---
+++ bin/functions.sh
@@ -0,0 +1,204 @@
+indentation="    "
+
+# Checks if the current directory is part of a .git repository
+function VerifyGitDirectory()
+{
+    if [[ ! -d .git && ! `git rev-parse --git-dir 2> /dev/null` ]]; then
+        PrintError "Not Git directory"
+        echo
+
+        exit
+    fi;
+}
+
+# Checks the given environment variable
+#
+# ${1} = Name of the environment variable
+# ${2} = Description of value
+function VerifyEnv()
+{
+    # Check environment variable.
+    envName=${1}
+    expectedValueDescription=${2}
+    expectedFile=${3}
+    export envVar=${envName}
+    eval envValue=\$$envVar
+
+    # We need to have the CDT security tools directory for access to the jwt tool.
+    if [[ ! -z "${envValue}" ]]; then
+        if [[ ! -z "${expectedFile}" ]]; then
+            exec 5>&1
+            results=$( VerifyFileExist "${expectedFile}" "${envValue}" | tee /dev/fd/5 )
+            if [[ -z $results ]]; then
+                return
+            fi
+        fi
+    else
+        PrintError "${envName} is not set!"
+        echo 
+    fi
+
+    PrintInfo "Set your ${envName} on the command line with:"
+    echo 
+    PrintCodeLine "export ${BICyan}${envName}${Cyan}=<${expectedValueDescription}>"
+    echo 
+
+    exit
+}
+
+# Prints a value intended to be for copying.
+#
+# ${1} = The value to paste.
+function PrintCopyPasta()
+{
+    pasta=${1}
+
+    # Print the token for output.
+    PrintSeparator
+    echo
+    PrintPasta "${pasta}"
+    echo
+    PrintSeparator
+    echo
+}
+
+# Add value to the clipboard.
+#
+# ${1} = Value to add to the clipboard.
+function AddToClipboard()
+{
+    value=${1}
+
+    # Add the token to the clipboard. Uses printf instead of echo to avoid extra newlines.
+    printf "${value}" | pbcopy
+
+    #echo "    ${BGreen}» Copied to clipboard.${No_Color}"
+    PrintBoldInfo "Copied to clipboard."
+    echo
+}
+
+# Symlinks a list of files to a target directory.
+#
+# ${1} = List of absolute paths to files for symlinking.
+# ${2} = Target directory to symlink to.
+function SymlinkFiles()
+{
+    srcFiles=${1}
+    targetPath=${2}
+
+    # For each runConfiguration in this directory.
+    for srcFilePath in ${srcFiles}; do
+        file=$(basename ${srcFilePath})
+        targetFilePath=${targetPath}/${file}
+
+        echo
+
+        # If the path is actually a symlink.
+        if [[ -L ${targetFilePath} ]]; then
+            # If it is the right symlink, then update it.
+            if [[ ${srcFilePath} = $(readlink ${targetFilePath}) ]]; then
+                PrintInfo "Found     ${BIBlue}${file}${Green}"
+                rm ${targetFilePath}
+                ln -s ${srcFilePath} ${targetFilePath}
+            # If it is the wrong symlink, then error out.
+            else
+                PrintError "  ${BRed}${file}${Red} is a symlink not from this script!"
+            fi
+            continue
+        fi
+
+        # If the path is an actual file, then error out.
+        if [[ -f ${targetFilePath} ]]; then
+            PrintError "  ${BRed}${file}${Red} is already an actual file!"
+            continue
+        fi
+
+        # Otherwise this is a new configuration so let's install it.
+        ln -s ${srcFilePath} ${targetFilePath}
+        PrintBoldInfo "Installed ${BIBlue}${file}${BGreen}"
+    done
+}
+
+function VerifyFileExist()
+{
+    file=${1}
+    dir=${2}
+
+    if [[ ! -f ${dir}/${file} ]]; then
+        PrintError "The file ${BRed}${file}${Red} does not exist in ${BRed}${dir}${Red}!"
+        echo
+    fi
+}
+
+function VerifyFolderExist()
+{
+    dir=${1}
+
+    if [[ ! -d ${dir} ]]; then
+        PrintError "The folder ${BRed}${dir}${Red} does not exist!"
+        echo
+    fi
+}
+
+function ReadInput()
+{
+    read -p $'\e[31m    Foobar\e[0m: ' foo
+}
+
+#################### Formatting Functions ####################
+function Indent()
+{
+    indentation="${indentation}    "
+}
+
+function Unindent()
+{
+    indentation="${indentation:0:${#indentation}-4}"
+}
+
+function PrintInfo()
+{
+    info=${1}
+    echo "${indentation}${Green}» ${info}${No_Color}"
+}
+
+function PrintBoldInfo()
+{
+    info=${1}
+    echo "${indentation}${BGreen}» ${info}${No_Color}"
+}
+
+function PrintCodeLine()
+{
+    code=${1}
+    echo "${indentation}    ${Cyan}${code}${No_Color}"
+}
+
+function PrintDescription()
+{
+    description=${1}
+    echo "${indentation}    ${description}"
+}
+
+function PrintWarning()
+{
+    warning=${1}
+    echo "${indentation}${BYellow}» ${warning}${No_Color}"
+}
+
+function PrintSeparator()
+{
+    echo "${Purple}--------------------------------------------------${No_Color}"
+}
+
+function PrintPasta()
+{
+    pasta=${1}
+    echo "${pasta}"
+}
+
+function PrintError()
+{
+    error=${1}
+    echo "${indentation}${Red}» ${White}${On_Red}[ERROR]${Red} ${error}${No_Color}"
+}
---
+++ bin/install.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# This gets the path of the executing script.
+SELF_PATH=$(cd -P -- "$(dirname -- "${0}")" && pwd -P) && SELF_PATH=${SELF_PATH}/$(basename -- "${0}")
+# If the path is a symlink (which is likely if it is in /usr/local/bin) this will get the actual filepath.
+REAL_FILE=$(readlink ${SELF_PATH})
+# If it isn't a symlink then let's stick with SELF_PATH
+if [[ -z $REAL_FILE ]]; then
+    REAL_FILE=${SELF_PATH}
+fi
+REAL_DIR=$(dirname ${REAL_FILE})
+
+source $REAL_DIR/colors.sh
+source $REAL_DIR/functions.sh
+
+# Start with a blank line.
+echo
+
+# Provide help.
+getopts h help
+if [[ "${help}" = "h" ]]; then
+    PrintInfo "Description"
+    echo
+    PrintDescription "Installs the following:"
+    echo
+    Indent
+    PrintDescription " - Utility bash scripts to ${BIBlue}/usr/local/bin${No_Color}"
+    Unindent
+    echo
+    PrintInfo "Usage"
+    echo
+    PrintCodeLine "${0} [-h]"
+    echo
+    PrintInfo "Examples"
+    echo
+    PrintCodeLine "${0}"
+    PrintCodeLine "${0} -h"
+    echo
+
+    exit
+fi
+
+# Install all utility bash scripts.
+PrintInfo "Installing bash utility scripts to ${BGreen}/usr/local/bin${Green}."
+Indent
+findCommand="find \`pwd\` -type f \( -name \"capi.create\" -o -name \"capi.delete\" \)"
+files=$(eval ${findCommand})
+SymlinkFiles "${files}" "/usr/local/bin"
+Unindent
+echo
